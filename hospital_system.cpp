cc8a3112081b9c8ed6be23f9fcf2dfc6abf197c7
mohamed ibrahim <mohammedkilany222@gmail.com>
2025-12-07 21:44:03 +0200

    add map and fix some methods

--- hospital_system.cpp
+++ hospital_system.cpp
@@ -3,36 +3,77 @@
 #include <string>
 #include "patient.h"
 #include "PatientQueue.h"
+#include "Doctor.h"
+#include "DoctorList.h"
 using namespace std;
 class HospitalSystem
 {
 private:
+    map<CaseType, vector<DoctorList>> doctorsByMajor;
     PatientQueue patientQueue;
 
 public:
     HospitalSystem() {}
 
-    void AddPatient(const Patient &p)
+    // ========= DOCTOR FUNCTIONS ============
+    void addDoctor();
+    void deleteDoctor();
+    void EditDoctor();
+    void getDoctorInfo();
+
+    // ========= PATIENT FUNCTIONS =========== ->>>DONE
+    void Addpatient(const Patient &p)
     {
         patientQueue.enqueue(p);
         cout << "Patient added: " << p.getName() << endl;
-    }
-
-    void ServePatient()
+    };
+    void deletePatient(const Patient &p)
     {
-        if (!patientQueue.isEmpty())
+        Patient removedPatient = patientQueue.dequeue();
+        if (removedPatient.getId() != 0)
         {
-            Patient p = patientQueue.next();
-            cout << "Serving patient: " << p.getName() << endl;
-            patientQueue.dequeue();
+            cout << "Patient removed: " << removedPatient.getName() << endl;
         }
         else
         {
-            cout << "No patients to serve." << endl;
+            cout << "No patient to remove." << endl;
         }
-    }
-
-    void ShowNextPatient()
+    };
+    void SearchById(int id)
+    {
+        Patient p = patientQueue.searchPatient(id);
+        if (p.getId() != 0)
+        {
+            cout << "Patient found: " << p.getName() << endl;
+            p.display();
+        }
+        else
+        {
+            cout << "Patient not found." << endl;
+        }
+    };
+    void SearchByPatient(const Patient &p)
+    {
+        Patient foundPatient = patientQueue.searchPatient(p);
+        if (foundPatient.getName() != "")
+        {
+            cout << "Patient found: " << foundPatient.getName() << endl;
+            foundPatient.display();
+        }
+        else
+        {
+            cout << "Patient not found." << endl;
+        }
+    };
+    void patientsCount()
+    {
+        cout << "Total patients in queue: " << patientQueue.getQueueCount() << endl;
+    };
+    void displayPatients()
+    {
+        patientQueue.display();
+    };
+    void showNext()
     {
         if (!patientQueue.isEmpty())
         {
@@ -45,24 +86,12 @@ public:
         {
             cout << "No patients in queue." << endl;
         }
-    }
+    };
 
-    void SearchPatient(int id)
-    {
-        Patient p = patientQueue.searchPatient(id);
-        if (p.getId() == -1)
-        {
-            cout << "Patient not found." << endl;
-        }
-        else
-        {
-            cout << "Patient found: " << p.getName() << endl;
-            p.display();
-        }
-    }
-
-    void ShowAllPatients()
-    {
-        patientQueue.display();
-    }
+    // ========= MENUS =======================
+    void doctorMenu();
+    void patientMenu();
+    void Menus();
+    void Welcome();
+    void exitSystem();
 };
